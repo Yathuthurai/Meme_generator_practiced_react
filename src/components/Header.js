b7d1940528c17970af3981e6945ba41d3f67c710
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-03-07 21:12:56 +0530

    Practiced web app ReactJS

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img
+             src="https://www.stickpng.com/assets/images/580b585b2edbce24c47b2a27.png"
+             alt="Problem?"
+            />
+            <p>MEME GENERATOR</p>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Myapp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
+function Myapp(){
+    return(
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default Myapp;
